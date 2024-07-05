40a4d6f0afb2900df7de52d46b1b3f0ef1264352
Thiago Souza <thiagopsouza23@gmail.com>
2024-07-05 19:35:32 -0300

    Modified directory structure. Added gitignore and README

---
+++ Codigo/cmake-build-debug/CMakeFiles/New_folder.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Programacao/Github/modelagem-membranas/Codigo/CalculadoraCuboPerfeitoDadosSaida.cpp" "CMakeFiles/New_folder.dir/CalculadoraCuboPerfeitoDadosSaida.cpp.obj" "gcc" "CMakeFiles/New_folder.dir/CalculadoraCuboPerfeitoDadosSaida.cpp.obj.d"
+  "C:/Programacao/Github/modelagem-membranas/Codigo/CalculadoraDadosSaida.cpp" "CMakeFiles/New_folder.dir/CalculadoraDadosSaida.cpp.obj" "gcc" "CMakeFiles/New_folder.dir/CalculadoraDadosSaida.cpp.obj.d"
+  "C:/Programacao/Github/modelagem-membranas/Codigo/Entrada/DadosEntradaModelo.cpp" "CMakeFiles/New_folder.dir/Entrada/DadosEntradaModelo.cpp.obj" "gcc" "CMakeFiles/New_folder.dir/Entrada/DadosEntradaModelo.cpp.obj.d"
+  "C:/Programacao/Github/modelagem-membranas/Codigo/Gerador/GeradorDadosSaida.cpp" "CMakeFiles/New_folder.dir/Gerador/GeradorDadosSaida.cpp.obj" "gcc" "CMakeFiles/New_folder.dir/Gerador/GeradorDadosSaida.cpp.obj.d"
+  "C:/Programacao/Github/modelagem-membranas/Codigo/Saida/DadosSaidaModelo.cpp" "CMakeFiles/New_folder.dir/Saida/DadosSaidaModelo.cpp.obj" "gcc" "CMakeFiles/New_folder.dir/Saida/DadosSaidaModelo.cpp.obj.d"
+  "C:/Programacao/Github/modelagem-membranas/Codigo/main.cpp" "CMakeFiles/New_folder.dir/main.cpp.obj" "gcc" "CMakeFiles/New_folder.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ README.md
@@ -0,0 +1,9 @@
+﻿# **modelagem-modulo-membranas**
+
+## UMLs:
+
+### Geometria:
+![Geometria](https://github.com/thpsouza/modelagem-modulo-membranas/blob/main/UML/Model!Geometria_0.png)
+
+### Mistura:
+![Mistura](https://github.com/thpsouza/modelagem-modulo-membranas/blob/main/UML/Model1!Mistura_1.png)
