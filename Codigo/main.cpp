a661ba1484fe9c0b84ec2d0f3713e0b1db0abe18
Thiago Souza <thiagopsouza23@gmail.com>
2024-07-05 22:02:05 -0300

    Created main function to test the code --SEGFAULT ERROR

--- Codigo/main.cpp
+++ Codigo/main.cpp
@@ -2,6 +2,22 @@
 // Created by LABCFD on 7/5/2024.
 //
 
+#include <iostream>
+#include "Entrada/DadosEntradaModelo.h"
+#include "Gerador/GeradorDadosSaida.h"
+#include "Saida/DadosSaidaModelo.h"
+
 int main() {
-    return 0;
+    double porosidade;
+    DadosEntradaModelo dadosEntrada {DadosEntradaModelo::CuboPerfeito, 0.8, 1};
+    DadosSaidaModelo dados;
+    GeradorDadosSaida gerador {&dadosEntrada};
+
+    /// SEGFAULT NO GETTER E NO SETTER DOS DADOS DE SAIDA
+    gerador.gerar();
+    dados = *gerador.getDadosSaida();
+    porosidade = dados.getPorosidade();
+
+    // Output
+    std::cout << "Porosidade: " << porosidade << std::endl;
 }
