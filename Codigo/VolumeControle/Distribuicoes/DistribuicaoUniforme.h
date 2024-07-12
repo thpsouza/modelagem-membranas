4cf269b83de0421445cf9c832552b3c847071542
Thiago Souza <thiagopsouza23@gmail.com>
2024-07-12 12:09:39 -0300

    Created distributions classes

---
+++ Codigo/VolumeControle/Distribuicoes/DistribuicaoUniforme.h
@@ -0,0 +1,17 @@
+//
+// Created by LABCFD on 7/12/2024.
+//
+
+#ifndef MODELAGEM_MEMBRANAS_DISTRIBUICAOUNIFORME_H
+#define MODELAGEM_MEMBRANAS_DISTRIBUICAOUNIFORME_H
+
+#include <cmath>
+#include "DistribuicaoBase.h"
+
+class DistribuicaoUniforme : public DistribuicaoBase {
+    /// Supondo fibras cilindricas, que não se sobrepoem, em uma geometria cúbica.
+    double porosidadeMaximaTeorica = M_PI * sqrt(3) / 6;
+};
+
+
+#endif //MODELAGEM_MEMBRANAS_DISTRIBUICAOUNIFORME_H
