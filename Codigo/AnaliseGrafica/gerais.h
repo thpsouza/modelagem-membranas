d1142e241b969b2657d4f4cbfb2dea5e06f3a976
Thiago Souza <thiagopsouza23@gmail.com>
2024-07-25 13:07:59 -0300

    Added a simple macro to facilitate printing to the console

--- Codigo/AnaliseGrafica/gerais.h
+++ Codigo/AnaliseGrafica/gerais.h
@@ -8,10 +8,19 @@
 #include <vector>
 #include <functional>
 #include <string>
+#include <iostream>
 
 std::vector<double> linspace(double start, double stop, int n = 50);
+
 std::vector<double> transform(std::vector<double> x, std::function<double(double)> f);
+
 void exportarDados(const std::string& path, const std::vector<std::string>& propriedades,
                    const std::vector<double>& X, const std::vector<std::vector<double>>& Y);
+
+template<typename ...Args>
+void print(Args && ...args) {
+    (std::cout << ... << args);
+}
+
 
 #endif //GERAIS_H
