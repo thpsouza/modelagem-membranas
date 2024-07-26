2f913e9fa5bea4d5eae944774587b5e7ea2c4154
Thiago Souza <thiagopsouza23@gmail.com>
2024-07-26 12:04:33 -0300

    Improved the graphical analysis

--- Codigo/AnaliseGrafica/analise.py
+++ Codigo/AnaliseGrafica/analise.py
@@ -1,52 +1,23 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import subprocess
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider, Button, RadioButtons
 
 
-def executar_cpp(build_command, run_command):
+def build_cpp(build_command):
     exit_code = subprocess.check_call(build_command)
-    if exit_code == 0:
-        # "[100%] Built target modelagem_membranas"
-        subprocess.check_call(run_command)
-    else:
-        print(exit_code)
+    if exit_code != 0:
+        raise Exception("Build error.\n")
 
 
-def ler_dados(path: str):
-    # with open(path, "r+") as file:
-    #    dados = file.read()
-    # return dados
-    df = pd.read_csv(path, sep=';')
-    return df
+def run_cpp(run_command):
+    exit_code = subprocess.check_call(run_command)
+    if exit_code != 0:
+        raise Exception("Run error.\n")
 
 
-def regredir_dados(dados: pd.DataFrame):
-    x = dados.iloc[:, 0]
-    Y = dados.iloc[:, 1:]
-    fitted = np.empty((Y.shape[1], 2))
-    for i, (_, y) in enumerate(Y.items()):
-        fitted[i] = np.polyfit(x, y.array, 1)
-    return fitted
-
-
-def plotar_dados(dados: pd.DataFrame, regressoes: np.ndarray, razao_cf_df: float):
-    legendas = [
-        "Porosidade\n" + f"f(x) = {round(regressoes[0][0],4)}*x + {round(regressoes[0][1],4)}",
-        "Número de fibras\n" + f"f(x) = {round(regressoes[1][0],4)}*x + {round(regressoes[1][1],4)}",
-        "Área total de transferência\n" + f"f(x) = {round(regressoes[2][0],4)}*x + {round(regressoes[2][1],4)}"
-    ]
-    axes = dados.plot(x=0, subplots=True, figsize=(10, 6),
-                      title=f"Variação das propriedades para um volume unitário, com cf = {razao_cf_df}df")
-    for i, ax in enumerate(axes):
-        ax.legend([legendas[i]])
-    plt.show()
-
-
-def main():
-    # Definir razão comprimento e diâmetro fibras:
-    cf_df = 10
-
+def obter_dados(cf_df: float, build=True) -> pd.DataFrame:
     # Importar path absoluto do cmake:
     with open("cmake_path.txt") as f:
         cmake_path = f.read()
@@ -55,13 +26,70 @@ def main():
     build_dir_path = "..\\cmake-build-debug"
     csv_file_path = "dados.csv"
     run_path = build_dir_path + "\\modelagem_membranas.exe"
-
     build_command = f'"{cmake_path}" --build "{build_dir_path}" --target modelagem_membranas -- -j 10'
     run_command = f'"{run_path}" "{csv_file_path}" "{cf_df}"'
 
-    executar_cpp(build_command, run_command)
-    dados = ler_dados(csv_file_path)
-    plotar_dados(dados, regredir_dados(dados), cf_df)
+    if build:
+        build_cpp(build_command)
+    run_cpp(run_command)
+    df = pd.read_csv(csv_file_path, sep=';')
+    return df
+
+
+def regredir_dados(dados: np.ndarray) -> np.ndarray:
+    x = dados[:, 0]
+    Y = dados[:, 1:].T
+    fitted = np.empty((Y.shape[1], 2))
+    for i, y in enumerate(Y):
+        fitted[i] = np.polyfit(x, y, 1)
+    return fitted
+
+
+def plotar_dados(dados: pd.DataFrame, razao_cf_df: float, interativo=False):
+    dados = dados.to_numpy()
+    construir_legendas = lambda regressoes: [
+        "Porosidade\n" + f"f(x) = {round(regressoes[0][0], 4)}*x + {round(regressoes[0][1], 4)}",
+        "Número de fibras\n" + f"f(x) = {round(regressoes[1][0], 4)}*x + {round(regressoes[1][1], 4)}",
+        "Área total de transferência\n" + f"f(x) = {round(regressoes[2][0], 4)}*x + {round(regressoes[2][1], 4)}"
+    ]
+
+    prop_cycle = plt.rcParams['axes.prop_cycle']
+    colors = prop_cycle.by_key()['color']
+    fig, axes = plt.subplots(3, 1, figsize=(10, 8), sharex=True)
+    fig.suptitle("Variação das propriedades para um volume unitário")
+
+    lines = []
+    for i, ax in enumerate(axes):
+        [line] = ax.plot(dados[:, 0], dados[:, i + 1], color=colors[i],
+                         label=[construir_legendas(regredir_dados(dados))[i]])
+        lines.append(line)
+        ax.legend()
+
+    if interativo:
+        fig.subplots_adjust(bottom=0.25)
+        slider_ax = fig.add_axes([0.185, 0.1, 0.65, 0.03], autoscale_on=True)
+        slider = Slider(slider_ax, 'cf/df', 1, 50.0, valinit=razao_cf_df)
+
+        def update_slider(val):
+            novos_dados = obter_dados(slider.val, build=False).to_numpy()
+            novo_Y = novos_dados[:, 1:]
+            for i, ax in enumerate(axes):
+                lines[i].set_ydata(novo_Y[:, i])
+                ax.legend([construir_legendas(regredir_dados(novos_dados))[i]])
+                ax.relim()
+                ax.autoscale_view()
+            fig.canvas.draw_idle()
+
+        slider.on_changed(update_slider)
+
+    plt.show()
+
+
+def main():
+    # Definindo a razão comprimento/diâmetro das fibras:
+    cf_df = 10
+    dados = obter_dados(cf_df)
+    plotar_dados(dados, cf_df, interativo=True)
 
 
 if __name__ == "__main__":
