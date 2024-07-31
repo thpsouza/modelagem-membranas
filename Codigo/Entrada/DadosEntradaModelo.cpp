f8855e63b342e6921e28e0d3fe52378af070b89a
Thiago Souza <thiagopsouza23@gmail.com>
2024-07-31 11:07:09 -0300

    Added doxygen documentation to the input data class

--- Codigo/Entrada/DadosEntradaModelo.cpp
+++ Codigo/Entrada/DadosEntradaModelo.cpp
@@ -1,9 +1,24 @@
-//
-// Created by LABCFD on 7/5/2024.
-//
+/**
+ * @file DadosEntradaModelo.cpp
+ * @author Thiago Souza (thiagosouza@eq.ufrj.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-07-05
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
 
 #include "DadosEntradaModelo.h"
 
+/**
+ * @brief Construtor da classe de entrada de dados.
+ * @param geometria Especificação da geometria do volume de controle.
+ * @param distribuicao Especificação do tipo de distribuição de fibras ao longo do volume de controle.
+ * @param empacotamento Fator de empacotamento das fibras no volume de controle.
+ * @param volume Volume do volume de controle.
+ * @param razaoComprimentoDiametroFibra Razão entre comprimento da fibra e do diâmetro da fibra (TEMPORÁRIO)/
+ */
 DadosEntradaModelo::DadosEntradaModelo(TipoGeometria geometria, TipoDistribuicao distribuicao,
                                        double empacotamento, double volume, double razaoComprimentoDiametroFibra):
         geometria(geometria),
@@ -14,18 +29,30 @@ DadosEntradaModelo::DadosEntradaModelo(TipoGeometria geometria, TipoDistribuicao
 {
 }
 
+/**
+ * @return Tipo de geometria do volume de controle.
+ */
 DadosEntradaModelo::TipoGeometria DadosEntradaModelo::getGeometria() const {
     return geometria;
 }
 
+/**
+ * @return Tipo de distribuição de fibras no volume de controle.
+ */
 DadosEntradaModelo::TipoDistribuicao DadosEntradaModelo::getDistribuicao() const {
     return distribuicao;
 }
 
+/**
+ * @return Fator de empacotamento das fibras no volume de controle.
+ */
 double DadosEntradaModelo::getEmpacotamento() const {
     return empacotamento;
 }
 
+/**
+ * @return Volume do volume de controle.
+ */
 double DadosEntradaModelo::getVolume() const {
     return volume;
 }
--- Codigo/Entrada/DadosEntradaModelo.h
+++ Codigo/Entrada/DadosEntradaModelo.h
@@ -1,11 +1,21 @@
-//
-// Created by LABCFD on 7/5/2024.
-//
+/**
+ * @file DadosEntradaModelo.h
+ * @author Thiago Souza (thiagosouza@eq.ufrj.com)
+ * @brief Arquivo com a classe de dados de entrada do programa.
+ * @version 0.1
+ * @date 2024-07-05
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
 
 #ifndef DADOSENTRADAMODELO_H
 #define DADOSENTRADAMODELO_H
 
-
+/**
+ * @brief Classe de entrada de dados para os cálculos do modelo. Aceita a geometria do volume de controle, a distribuição
+ * de fibras, tal como o empacotamento e volume do VC.
+ */
 class DadosEntradaModelo{
 public:
     enum TipoGeometria
@@ -22,10 +32,10 @@ public:
     DadosEntradaModelo(TipoGeometria geometria, TipoDistribuicao distribuicao,
                        double empacotamento, double volume, double razaoComprimentoDiametroFibra);
 
-    TipoGeometria getGeometria() const;
-    TipoDistribuicao getDistribuicao() const;
-    double getEmpacotamento() const;
-    double getVolume() const;
+    [[nodiscard]] TipoGeometria getGeometria() const;
+    [[nodiscard]] TipoDistribuicao getDistribuicao() const;
+    [[nodiscard]] double getEmpacotamento() const;
+    [[nodiscard]] double getVolume() const;
     double razaoComprimentoDiametroFibra;
 
 private:
