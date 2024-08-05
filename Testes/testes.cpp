98ea20874c1e8c9872aa72d26dd9d8b2dcd4f29e
Thiago Souza <thiagopsouza23@gmail.com>
2024-08-05 11:30:00 -0300

    Added tests folder

---
+++ Testes/testes.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file testes.cpp
+ * @author Thiago Souza (thiagosouza@eq.ufrj.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-08-05
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+
+int main() {
+    return 0;
+}
